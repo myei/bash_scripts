2a24401da04c550d3e5ba851371a6f83d656f57c
Manuel Gil <manuelyeixon@gmail.com>
2016-08-18 09:56:17 -0400

    Mejorado el autenticateGit

--- autenticateGit.sh
+++ autenticateGit.sh
@@ -21,6 +21,15 @@ RED='\033[0;31m'
 CYAN='\033[0;36m'
 NC='\033[0m'
 
+##############################################################
+#		V A L I D A C I O N E S   G E N E R A L E S
+##############################################################
+
+if [ $EUID -ne 0 ]; then
+	printf "${RED} -- ERROR: Debes ejecutar este script como root --${NC}\n"
+	exit 1
+fi
+
 if [ ! -f LB-PMO.pem ]; then
 	printf "${RED} -- ERROR: Debes ejecutar este script desde la misma ruta donde se encuentra el certificado LB-PMO.pem --${NC}\n"
 	exit 1
@@ -33,19 +42,23 @@ if [ ! -d /home/$user ]; then
 	exit 1
 fi
 
-if [ ! -d /home/$user/certs ]; then
-	mkdir /home/$user/certs
+##############################################################
+#	  R E G I S T R A N D O   E L   C E R T I F I C A D O
+##############################################################
+
+if [ ! -d /home/$user/.certs ]; then
+	mkdir /home/$user/.certs
 fi
 
 if [ ! -f /etc/ssh/ssh_config ]; then
 	touch /etc/ssh/ssh_config
 fi
 
-cp LB-PMO.pem /home/$user/certs/
-chmod 600 /home/$user/certs/LB-PMO.pem
-chown -R $user:$user /home/$user/certs
+cp LB-PMO.pem /home/$user/.certs/
+chmod 600 /home/$user/.certs/LB-PMO.pem
+chown -R $user:$user /home/$user/.certs
 
-echo -e "\nIdentityFile /home/$user/certs/LB-PMO.pem" >> /etc/ssh/ssh_config
+echo -e "\n IdentityFile /home/$user/.certs/LB-PMO.pem" >> /etc/ssh/ssh_config
 
 printf "${CYAN} -- Certificado registrado satisfactoriamente --${NC}\n"
 exit
