7dc55d57ceda549e6ca5af8ae76b6aad9a5a3ed7
Manuel Gil <manuelyeixon@gmail.com>
2017-04-28 12:22:16 -0400

    mejorando mongo backup, recibiendo lista de base de datos

--- db/mongo-backup.sh
+++ db/mongo-backup.sh
@@ -21,9 +21,12 @@ HOST="localhost"
 if [[ $# -eq 0 ]]; then
 	printf "error: Argumentos inválidos \n\n"
 
-	printf "mongo-backup usage: mongo-backup [ dbname1 dbname2 ... dbnameN ] \n\n"
+	printf 'mongo-backup usage: mongo-backup [ OPTIONS ] DB-LIST: [ "dbname1 dbname2 ... dbnameN" ] \n\n'
 
-	printf "	dbnameX: Nombre de las bases de datos a respaldar \n\n"
+    printf "    OPTIONS: \n\n"
+    printf "    -p, --port: Puerto, por defecto 27017 \n"
+    printf "    -h, --host: Nombre de las bases de datos a respaldar \n"
+    printf "    -d, --db:   DB-LIST -> Nombre de las bases de datos a respaldar, deben ir dentro de comillas (\") y separados por espacios \n"
 
 	exit
 fi
@@ -48,23 +51,23 @@ case $key in
     ;;
     *)
     ;;
-esac
-shift
+    esac
+    shift
 done
 
 mkdir -p $DIR
 
-for db in $DB; do
+for db in ${DB[*]}; do
 
 	printf "Respaldando ${db} en ${DIR}${db}_`date +%d-%m-%Y` \n"
 	
-	# REALIZO EL BACKUP A LA BASE DE DATOS DE PRODUCCION
+	# REALIZO EL BACKUP A LA BASE DE DATOS
 	mongodump --host $HOST --port $PORT --db $db --out $DIR$db'_'`date +%d-%m-%Y`
 
 	printf "Restaurando respaldo en ${db}_backup \n"
 
 	# EJECUTO EL RESTORE CREADO ANTERIORMENTE EN LA BASE DE DATOS DE RESPALDO
-	mongorestore --port $PORT --db $db"_backup" $DIR$db'_'`date +%d-%m-%Y`"/$db"
+	mongorestore --host $HOST --port $PORT --db $db"_backup" $DIR$db'_'`date +%d-%m-%Y`"/$db"
 
 done
 
