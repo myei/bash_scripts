5eff24e7c632b68092918d955b71a369f379d63d
Manuel Gil <mauelyeixon@gmail.com>
2017-11-19 17:16:54 -0400

    sending mail...

--- util/overload_checker.sh
+++ util/overload_checker.sh
@@ -4,10 +4,12 @@ log=/var/log/overload.log
 cpu=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
 memory=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
 
-if [[ $cpu > 90 ]]; then
-	echo "cpu usage: $cpu%" >> $log
+if [[ $cpu > 80 ]]; then
+	echo "`date -u +%Y-%m-%dT%H:%M:%S` cpu usage: $cpu%" >> $log
+	echo "`date -u +%Y-%m-%dT%H:%M:%S` cpu usage: $cpu%" | $(mail -s "WARN DED779" manuelyeixon@gmail.com)
 fi
 
-if [[ $memory > 90 ]]; then
-	echo "memory usage: $memory%" >> $log
+if [[ $memory > 80 ]]; then
+	echo "`date -u +%Y-%m-%dT%H:%M:%S` memory usage: $memory%" >> $log
+	echo "`date -u +%Y-%m-%dT%H:%M:%S` memory usage: $memory%" | $(mail -s "WARN DED779" manuelyeixon@gmail.com)
 fi
