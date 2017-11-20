1045c1bc058f410f3f31e768f0be217be10d334c
Manuel Gil <manuelyeixon@gmail.com>
2017-11-20 10:26:32 -0400

    especificando porcentaje a monitorear..

--- util/overload_checker.sh
+++ util/overload_checker.sh
@@ -4,12 +4,17 @@ log=/var/log/overload.log
 cpu=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
 memory=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
 
-if [[ $cpu > 80 ]]; then
+if [[ ${#} == 0 ]]; then
+	echo "Debe especificar el porcentaje base a monitorear..."
+	exit 1
+fi
+
+if [[ ${cpu%.*} > $1 ]]; then
 	echo "`date -u +%Y-%m-%dT%H:%M:%S` cpu usage: $cpu%" >> $log
 	echo "`date -u +%Y-%m-%dT%H:%M:%S` cpu usage: $cpu%" | $(mail -s "WARN DED779" manuelyeixon@gmail.com)
 fi
 
-if [[ $memory > 80 ]]; then
+if [[ ${memory%.*} > $1 ]]; then
 	echo "`date -u +%Y-%m-%dT%H:%M:%S` memory usage: $memory%" >> $log
 	echo "`date -u +%Y-%m-%dT%H:%M:%S` memory usage: $memory%" | $(mail -s "WARN DED779" manuelyeixon@gmail.com)
 fi
