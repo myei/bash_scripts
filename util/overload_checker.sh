623565c93e309f778cc773fda262a71ac4f8cd04
Manuel Gil <manuelyeixon@gmail.com>
2017-12-06 15:49:19 -0400

    adding auto-reload apache...

--- util/overload_checker.sh
+++ util/overload_checker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SERVER=$(hostname)
+EMAIL='manuelyeixon@gmail.com'
+
 log=/var/log/overload.log
 cpu=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')
 memory=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
@@ -11,10 +14,21 @@ fi
 
 if [[ ${cpu%.*} -gt $1 ]]; then
 	echo "`date -u +%Y-%m-%dT%H:%M:%S` cpu usage: $cpu%" >> $log
-	echo "`date -u +%Y-%m-%dT%H:%M:%S` cpu usage: $cpu%" | $(mail -s "WARN DED779" manuelyeixon@gmail.com)
+	echo "`date -u +%Y-%m-%dT%H:%M:%S` cpu usage: $cpu%" | $(mail -s "WARN ${SERVER}" $EMAIL)
 fi
 
 if [[ ${memory%.*} -gt $1 ]]; then
 	echo "`date -u +%Y-%m-%dT%H:%M:%S` memory usage: $memory%" >> $log
-	echo "`date -u +%Y-%m-%dT%H:%M:%S` memory usage: $memory%" | $(mail -s "WARN DED779" manuelyeixon@gmail.com)
+
+	if [[ ${memory%.*} -gt 80 ]]; then
+		service apache2 stop
+		sleep 5
+		service apache2 start
+
+		echo "`date -u +%Y-%m-%dT%H:%M:%S` memory usage: $memory%... Apache fue reiniciado" | $(mail -s "WARN ${SERVER}" $EMAIL)
+
+		exit
+	fi
+	
+	echo "`date -u +%Y-%m-%dT%H:%M:%S` memory usage: $memory%" | $(mail -s "WARN ${SERVER}" $EMAIL)
 fi
