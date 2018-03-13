195296304165e7632555bdc871f6b3de7ea56a2a
Manuel Gil <mauelyeixon@gmail.com>
2018-03-12 21:55:12 -0400

    updated to python3.6.4

--- util/installers/inst_python3_jessie.sh
+++ util/installers/inst_python3_jessie.sh
@@ -4,16 +4,16 @@
 apt install -y build-essential libc6-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 # get source code
 cd $HOME
-wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
-tar -zxvf Python-3.6.1.tgz
-cd Python-3.6.1
+wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
+tar -zxvf Python-3.6.4.tgz
+cd Python-3.6.4
 # build & install
 ./configure
 make -j4          # using 4 threads for some speed
 make install
 # cleanup
 cd ..
-rm -fr ./Python-3.6.1*
+rm -fr ./Python-3.6.4*
 # upgrade (just in case)
 pip3 install -U pip
 pip3 install -U setuptools
