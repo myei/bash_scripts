1bd3b62ecd7edc71552d662875cf8c2eeb1e7d97
Manuel Gil <manuelyeixon@gmail.com>
2018-07-16 11:06:41 -0400

    mongo 3 -> 4

--- util/installers/inst_mongo_community_3.6.sh
+++ util/installers/inst_mongo_community_3.6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.6 main" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 apt update
 apt install mongodb-org -y
 mongo --version
