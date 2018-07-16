44c64f58a6a1247914c4e268e34163d9c1564f15
Manuel Gil <manuelyeixon@gmail.com>
2018-07-16 10:27:04 -0400

    actualizando llave y actualizando python3

--- util/installers/inst_python3_jessie.sh
+++ util/installers/inst_python3_jessie.sh
@@ -4,16 +4,16 @@
 apt install -y build-essential libc6-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 # get source code
 cd $HOME
-wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
-tar -zxvf Python-3.6.4.tgz
-cd Python-3.6.4
+wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
+tar -zxvf Python-3.7.0.tgz
+cd Python-3.7.0
 # build & install
-./configure
+./configure --enable-shared
 make -j4          # using 4 threads for some speed
 make install
 # cleanup
 cd ..
-rm -fr ./Python-3.6.4*
+rm -fr ./Python-3.7.0*
 # upgrade (just in case)
 pip3 install -U pip
 pip3 install -U setuptools
