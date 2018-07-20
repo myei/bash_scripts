87e39c88d5e289fc8f13303528953144e5d3ed6b
Manuel Gil <manuelyeixon@gmail.com>
2018-07-20 12:56:02 -0400

    mejorando...

--- util/wp-maker.py
+++ util/wp-maker.py
@@ -1,7 +1,7 @@
 from sys import argv as args
 from sqlalchemy import create_engine
 from blessings import Terminal
-from os import system
+from os import system, makedirs
 
 import random
 import string
@@ -9,33 +9,31 @@ import string
 
 class WPMaker:
 
-    _APACHE = {
-        'PATH': '/var/www/html/',
-        'PORT': 80,
-        'SA': '/etc/apache2/sites-available/'
+    APACHE = {
+        'path': '/var/www/html/',
+        'port': 80,
+        'sa': '/etc/apache2/sites-available/'
     }
 
-    _DRIVER = 'mysql'
+    DB = {
+        'user': 'root',
+        'psw': '12345',
+        'host': 'localhost',
+        'port': 3306,
+        'driver': 'mysql'
+    }
 
     def __init__(self, site_name):
-        self._user = 'root'
-        self._psw = '12345'
-        self._host = 'localhost'
-        self._port = 3306
         self._name = site_name
         self._db = self._name.split('.')[0]
 
     def _connect(self):
         try:
-            engine = create_engine('{driver}://{user}:{psw}@{host}:{port}/{driver}'.format(user=self._user,
-                                                                                           psw=self._psw,
-                                                                                           host=self._host,
-                                                                                           port=self._port,
-                                                                                           driver=self._DRIVER))
+            engine = create_engine('{driver}://{user}:{psw}@{host}:{port}/{driver}'.format(**self.DB))
             self._conn = engine.connect()
             self._conn.execute('commit')
         except Exception as e:
-            print(t.bold_red('Error trying to connect to {}, please verify your credentials'.format(self._DRIVER)))
+            print(t.bold_red('Error trying to connect to {}, please verify your credentials'.format(self.DB['driver'])))
             exit(1)
 
     def _build_db(self):
@@ -45,50 +43,75 @@ class WPMaker:
             self._psw = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(18)])
             self._psw = self._psw.replace('%', '').replace('\\', '').replace('"', '')
 
-            self._conn.execute('create database {}'.format(self._db))
-            self._conn.execute('CREATE USER {}@{} IDENTIFIED BY "{}"'.format(self._db, self._host, self._psw))
-            self._conn.execute('GRANT ALL PRIVILEGES ON {db} . * TO {db}@{host}'.format(db=self._db, host=self._host))
-
-            self._conn.close()
+            self._conn.execute('CREATE DATABASE {}'.format(self._db))
+            self._conn.execute('CREATE USER {}@{} IDENTIFIED BY "{}"'.format(self._db, self.DB['host'], self._psw))
+            self._conn.execute('GRANT ALL PRIVILEGES ON {db} . * TO {db}@{host}'.format(db=self._db, host=self.DB['host']))
         except Exception as e:
-            print(t.bold_red('Error trying to build database'))
+            print(t.bold_red('Error trying to build database'), e.args)
             exit(2)
+
+    def _rollback_db(self):
+        self._conn.execute('DROP DATABASE {}'.format(self._db))
+        self._conn.execute('DROP USER {}@{}'.format(self._db, self.DB['host']))
 
     def _build_apache(self):
         try:
-            with open('{}{}.conf'.format(self._APACHE['SA'], self._name), 'w') as vh:
-                vh.write('<VirtualHost *:{PORT}> \n'
+            makedirs(self.APACHE['path'] + self._name, exist_ok=True)
+
+            with open('{}{}.conf'.format(self.APACHE['sa'], self._name), 'w') as vh:
+                vh.write('<VirtualHost *:{port}> \n'
                          '\tServerAdmin webmaster@getout.com \n'
                          '\tServerName {page} \n'
                          '\tServerAlias www.{page} \n\n'
-                         '\tDocumentRoot {PATH}{page} \n\n'
+                         '\tDocumentRoot {path}{page} \n\n'
                          '\t<Directory /> \n'
                          '\t\tOptions FollowSymLinks \n'
                          '\t\tAllowOverride All \n'
                          '\t</Directory> \n'
-                         '\t<Directory {PATH}{page}> \n'
+                         '\t<Directory {path}{page}> \n'
                          '\t\tOptions Indexes FollowSymLinks MultiViews \n'
                          '\t\tAllowOverride All \n'
                          '\t\tOrder allow,deny \n'
                          '\t\tallow from all \n'
                          '\t</Directory> \n'
-                         '</VirtualHost>'.format(page=self._name, **self._APACHE))
+                         '</VirtualHost>'.format(page=self._name, **self.APACHE))
 
             system('a2ensite {} 1>/dev/null'.format(self._name))
             system('service apache2 restart 2>/dev/null')
         except Exception as e:
-            print(t.bold_red('Error trying to build apache context:'), e.args)
+            self._rollback_db()
+            print(t.bold_red('Error trying to build apache context, rolling back database changes...'), e.args)
             exit(3)
 
+    @staticmethod
+    def yes_or_no(question):
+        try:
+            choices = {'y': True, 'n': False}
+            choice = str(input(t.bold_yellow(question + ' (y/n): '))).lower().strip()
+
+            return choices.get(choice) if choice in choices else WPMaker.yes_or_no(question)
+        except:
+            print(t.bold_red('\n\nERROR: you must type y or n'))
+            exit(4)
+
+    def _clean(self):
+        self._conn.close()
+        system('clear')
+
     def make(self):
+        if not self.yes_or_no('Are you sure about creating this site: ' + t.bold_cyan_italic(self._name) + '?'):
+            exit(5)
+
         print(t.bold_blue('Building site...'))
 
         self._build_db()
         self._build_apache()
 
-        system('clear')
+        self._clean()
+
         print(t.bold_green('Successfully created context for:\n'))
-        print(t.bold('  Site:'), t.bold_cyan('\t' + self._name), '\n')
+        print(t.bold('  Site:'), t.bold_cyan('\t' + self._name))
+        print(t.bold('  Path:'), t.bold_cyan('\t' + self.APACHE['path'] + self._name), '\n')
         print('', t.bold_underline('Database:\n'))
         print(t.bold('  User:'), t.bold_yellow('\t' + self._db))
         print(t.bold('  Password:'), t.bold_yellow('\t' + self._psw), '\n')
