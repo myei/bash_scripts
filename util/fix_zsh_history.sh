e48ef6958b9323c8095df7d74472c14e4761f1f5
mgil <mgil@smartersolutions.com.ve>
2018-08-23 10:36:29 -0400

    optimizando

--- util/fix_zsh_history.sh
+++ util/fix_zsh_history.sh
@@ -1,5 +1,7 @@
-#! /bin/bash
+#!/bin/sh
+# Fixs corrupt .zsh_history
 
-mv .zsh_history .zsh_history_bad
-strings .zsh_history_bad > .zsh_history
-fc -R .zsh_history
+mv ~/.zsh_history ~/.zsh_history_bad
+strings ~/.zsh_history_bad > ~/.zsh_history
+fc -R ~/.zsh_history
+rm ~/.zsh_history_bad
