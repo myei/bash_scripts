e8f07f8ae5577c6520abbd5b7a6a815148567d60
mgil <mgil@initiumsoft.com>
2021-06-21 17:20:23 -0400

    new capabilities added
    - composer update 
    - schema update
    - improving readability

--- git/auto-deployment.sh
+++ git/auto-deployment.sh
@@ -14,12 +14,16 @@
 # - Permite auto instalación (-i --install)
 # 	
 #
-# 														v2.2
+# 														v2.3
 ##############################################################
 
+# C O N F I G U R A T I O N
 SCRIPT_NAME='auto-deployment'
 PROJECT_USER='apache'
 PHP_PATH=php
+COMPOSER_PATH=/usr/local/bin/composer
+COMPOSER_LOCATION='composer.json'
+ENTITY_PREFIX='Entity/'
 
 # C O L O R S
 GREEN='\033[0;32m'
@@ -59,6 +63,57 @@ elif [[ $1 = "-i" || $1 = "--install" ]]; then
 	exit
 fi
 
+
+# F U N C T I O N S 
+clearCache () {
+	if [[ -f bin/console ]]; then
+		if [[ -f .env ]]; then
+			printf "${YELLOW}${BOLD}\nsymfony project detected [>= v4], clearing cache...${NC} \n"
+			$PHP_PATH bin/console cache:clear
+		else
+			printf "${YELLOW}${BOLD}\nsymfony project detected [<= v3], clearing cache...${NC} \n"
+			rm -rf var/cache/* && $PHP_PATH bin/console cache:clear --env=prod --no-warmup
+			$PHP_PATH bin/console assets:install --symlink --relative web 
+			chown -R $PROJECT_USER:$PROJECT_USER .
+			setfacl -d -m u:$PROJECT_USER:rwx -m u:`whoami`:rwx var/{cache,logs,sessions} web && setfacl -dR -m u:$PROJECT_USER:rwx -m u:`whoami`:rwx var/{cache,logs,sessions} web
+		fi
+	fi
+}
+
+composerUpdate () {
+	if echo $NEW_CHANGES | grep -q $COMPOSER_LOCATION; then
+		printf "${YELLOW}${BOLD}\nRunning composer update ${NC} \n"
+		$PHP_PATH $COMPOSER_PATH update
+
+		successCU=$?
+		if [ $successCU -eq 0 ]; then
+			printf "${GREEN}${BOLD}Composer update executed successfully ${NC} \n"
+		else
+			printf "${RED}${BOLD}Something went wrong! Composer update not executed ${NC} \n"
+		fi
+	else
+		printf "${GREEN}${BOLD}Composer update not needed... ${NC} \n"
+	fi
+}
+
+schemaUpdate () {
+	if echo $NEW_CHANGES | grep -q $ENTITY_PREFIX; then
+		printf "${YELLOW}${BOLD}\nRunning schema update ${NC} \n"
+		$PHP_PATH bin/console doctrine:schema:update --force
+
+		successUDB=$?
+		if [ $successUDB -eq 0 ]; then
+			printf "${GREEN}${BOLD}Database schema update executed successfully ${NC} \n"
+		else
+			printf "${RED}${BOLD}Something went wrong! Database schema update not executed ${NC} \n"
+		fi
+	else
+		printf "${GREEN}${BOLD}Schema update not needed... ${NC} \n"
+	fi
+}
+# / F U N C T  I O N S
+
+# M A I N
 src=`pwd $(readlink -f "$0")`
 
 for project in $@; do
@@ -67,7 +122,7 @@ for project in $@; do
 
 		cd $project
 
-		printf "updating -> ${CYAN}${BOLD}`basename $project`${NC} listening on: ${YELLOW}${BOLD}${I}`git rev-parse --abbrev-ref HEAD`${NC}... \n"
+		printf "updating -> ${CYAN}${BOLD}$(basename $(realpath $project))${NC} listening on: ${YELLOW}${BOLD}${I}`git rev-parse --abbrev-ref HEAD`${NC}... \n"
 		git remote update
 
 		UPSTREAM=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
@@ -75,8 +130,9 @@ for project in $@; do
 	    REMOTE=$(git rev-parse $UPSTREAM)
 	    BASE=$(git merge-base @ $UPSTREAM)
 		WD_CHANGES=$(git status --untracked-files=no --porcelain)
+		NEW_CHANGES=$(git diff --name-only --merge-base $LOCAL $REMOTE)
 
-		printf "status: "
+		printf "\nstatus: "
 		if [[ $LOCAL = $REMOTE ]]; then
 		    printf "${GREEN}${BOLD}up-to-date${NC} \n\n"
 		elif [[ $LOCAL = $BASE ]]; then
@@ -85,30 +141,23 @@ for project in $@; do
 			if [[ ${#WD_CHANGES} = 0 ]]; then
 		    	printf "${GREEN}${BOLD}working directory clean...${NC} \n"
 			else
-		    	printf "${YELLOW}${BOLD}working directory is dirty, stashing...${NC} \n"
+		    	printf "${YELLOW}${BOLD}\nworking directory is dirty, stashing...${NC} \n"
 				git stash
 			fi
 
 			git pull
 			PULL_STATUS=$?
 
+			composerUpdate
+
+			schemaUpdate
+
 			if [[ ${#WD_CHANGES} > 0 ]]; then
-		    	printf "${YELLOW}${BOLD}bringing back stashed changes...${NC} \n"
+		    	printf "${YELLOW}${BOLD}\nbringing back stashed changes...${NC} \n"
 				git stash pop
 			fi
 
-			if [[ -f bin/console ]]; then
-				if [[ -f .env ]]; then
-		    		printf "${YELLOW}${BOLD}symfony project detected [>= v4], clearing cache...${NC} \n"
-					$PHP_PATH bin/console cache:clear
-				else
-		    		printf "${YELLOW}${BOLD}symfony project detected [<= v3], clearing cache...${NC} \n"
-					rm -rf var/cache/* && $PHP_PATH bin/console cache:clear --env=prod --no-warmup
-					$PHP_PATH bin/console assets:install --symlink --relative web 
-					chown -R $PROJECT_USER:$PROJECT_USER .
-					setfacl -d -m u:$PROJECT_USER:rwx -m u:`whoami`:rwx var/{cache,logs,sessions} web && setfacl -dR -m u:$PROJECT_USER:rwx -m u:`whoami`:rwx var/{cache,logs,sessions} web
-				fi
-			fi
+			clearCache
 
 			if [[ $PULL_STATUS = 0 ]]; then 
 				printf "status: ${GREEN}${BOLD}updated${NC} \n\n"
